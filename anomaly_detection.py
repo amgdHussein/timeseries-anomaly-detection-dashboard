0ce575affedd5d01cf4eee7253a6764f0666a8bc
Amgd2112 <amgad.hussein99.com>
2021-06-20 11:50:51 +0200

    dashboard design

--- anomaly_detection.py
+++ anomaly_detection.py
@@ -12,19 +12,20 @@ from fbprophet.plot import plot_components_plotly
 # error metrics
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, median_absolute_error
 
+
 class ADetector(object):
     def __init__(self, name):
         self.NAME = name
         self.METRICS = {
-            'MAE':mean_absolute_error,
-            'R2':r2_score, 
-            'MSE':mean_squared_error,
-            'MedAE':median_absolute_error,
+            'MAE': mean_absolute_error,
+            'R2': r2_score,
+            'MSE': mean_squared_error,
+            'MedAE': median_absolute_error,
         }
         self.period = 30
-        
+
     def update_model(self, df, params):
-        self.params=params
+        self.params = params
         self.model_init_date = df.ds.iloc[-1]
 
         daily = 'D' in self.params
@@ -32,7 +33,11 @@ class ADetector(object):
         yearly = 'Y' in self.params
 
         if daily or weekly or yearly:
-            self.model = Prophet(daily_seasonality=daily, weekly_seasonality=weekly, yearly_seasonality=yearly)
+            self.model = Prophet(
+                daily_seasonality=daily,
+                weekly_seasonality=weekly,
+                yearly_seasonality=yearly,
+            )
         else:
             self.model = Prophet()
 
@@ -42,7 +47,11 @@ class ADetector(object):
 
     def future_dataframe(self, period):
         self.period = period
-        data = self.model.make_future_dataframe(periods=self.period, freq='D', include_history=True)
+        data = self.model.make_future_dataframe(
+            periods=self.period,
+            freq='D',
+            include_history=True,
+        )
         self.dataframe_forecast = self.model.predict(df=data)
 
     def predict_dataframe(self, series):
@@ -51,10 +60,10 @@ class ADetector(object):
         self.dataframe_predict['y'] = series['y']
         self.classify_anomaly()
 
-
     def classify_anomaly(self, stds=[2, 4, 8]):
         # populate errors
-        self.dataframe_predict['residuals'] = self.dataframe_predict['y'] - self.dataframe_predict['yhat']
+        self.dataframe_predict['residuals'] = self.dataframe_predict['y'] - \
+            self.dataframe_predict['yhat']
         error = self.dataframe_predict.residuals.abs()
         mean_of_errors = error.values.mean()
         std_of_errors = error.values.std(ddof=0)
@@ -83,32 +92,40 @@ class ADetector(object):
         )
 
         predicted_line = go.Scatter(
-            x=self.dataframe_forecast.ds, 
-            y=self.dataframe_forecast.yhat, 
-            mode='lines', 
-            name='Prediction', 
+            x=self.dataframe_forecast.ds,
+            y=self.dataframe_forecast.yhat,
+            mode='lines',
+            name='Prediction',
             line_color='RoyalBlue',
         )
 
         confidence_area = go.Scatter(
-            x=pd.concat([self.dataframe_forecast.ds, self.dataframe_forecast.ds[::-1]]),
-            y=pd.concat([self.dataframe_forecast.yhat_upper, self.dataframe_forecast.yhat_lower[::-1]]),
+            x=pd.concat([
+                self.dataframe_forecast.ds,
+                self.dataframe_forecast.ds[::-1]
+            ]),
+            y=pd.concat([
+                self.dataframe_forecast.yhat_upper,
+                self.dataframe_forecast.yhat_lower[::-1]
+            ]),
             mode='none',
             name='Confidence Interval',
             fill='toself',
             fillcolor='RoyalBlue',
             opacity=0.2,
         )
-        
+
         data = [scatter_normal, predicted_line, confidence_area]
         colors = ['#F4D03F', '#F39C12', '#CB4335']
         for i in range(len(colors)):
-            anomalies = self.dataframe_predict[self.dataframe_predict.impact==i+1]
+            anomalies = self.dataframe_predict[
+                self.dataframe_predict.impact == i+1
+            ]
             scatter_anomaly = go.Scatter(
-                x=anomalies.ds, 
-                y=anomalies.y, 
-                mode='markers', 
-                name=f'Actual-Anomaly-Impact {i+1}', 
+                x=anomalies.ds,
+                y=anomalies.y,
+                mode='markers',
+                name=f'Actual-Anomaly-Impact {i+1}',
                 marker=dict(color=colors[i], size=4)
             )
             data.append(scatter_anomaly)
@@ -117,15 +134,33 @@ class ADetector(object):
             hovermode='x',
             template='plotly_dark',
             showlegend=True,
-            title=dict(text=f'{series_name} Streaming', font=dict(size=20)),
+            title=dict(
+                text=f'{series_name} Streaming',
+                font=dict(size=20),
+            ),
             yaxis=dict(title=self.NAME),
             xaxis=dict(
                 title='Date-Time (Day)',
                 rangeselector=dict(
                     buttons=list([
-                        dict(count=1, label='1D', step='day', stepmode='backward'),
-                        dict(count=6, label='1M', step='month', stepmode='backward'),
-                        dict(count=1, label='1Y', step='year', stepmode='backward'),
+                        dict(
+                            count=1,
+                            label='1D',
+                            step='day',
+                            stepmode='backward',
+                        ),
+                        dict(
+                            count=6,
+                            label='1M',
+                            step='month',
+                            stepmode='backward',
+                        ),
+                        dict(
+                            count=1,
+                            label='1Y',
+                            step='year',
+                            stepmode='backward',
+                        ),
                         dict(step='all'),
                     ]),
                     font=dict(color='black'),
@@ -144,8 +179,8 @@ class ADetector(object):
 
     def hist_plot(self):
         fig = px.histogram(
-            data_frame=self.dataframe_predict, 
-            x='y', 
+            data_frame=self.dataframe_predict,
+            x='y',
             marginal='box',
         )
 
@@ -166,39 +201,61 @@ class ADetector(object):
         fig.update_layout(layout)
         fig.update_traces(
             marker=dict(
-                color='RoyalBlue', 
-                opacity=0.4, 
+                color='RoyalBlue',
+                opacity=0.4,
                 line=dict(color='#9370db', width=1.3)
             )
         )
-        
+
         return fig
-    
+
     def seasonal_components_plot(self):
         fig = plot_components_plotly(self.model, self.dataframe_predict)
         fig.update_traces(line=dict(color=' RoyalBlue'))
         fig.update_layout(
-            template='plotly_dark', 
+            template='plotly_dark',
             title=dict(text='Seasonal Components', font=dict(size=20)),
-            height=None, 
-            width=None, 
+            height=None,
+            width=None,
             margin=dict(b=10, r=10, l=10),
         )
         return fig
-        
-    error = lambda self, metric: metric(self.dataframe_predict.y, self.dataframe_predict.yhat)
+
+    def error(self, metric): return metric(
+        self.dataframe_predict.y,
+        self.dataframe_predict.yhat
+    )
+
     def metric_plot(self):
-        metrics_results = {name:self.error(method) for name, method in self.METRICS.items()}
-        
+        metrics_results = {
+            name: self.error(method)
+            for name, method in self.METRICS.items()
+        }
+
         error_df = pd.DataFrame(metrics_results, index=[0])
-        error_df = error_df.melt(var_name='Metric', value_name='Error')
+        error_df = error_df.melt(
+            var_name='Metric',
+            value_name='Error',
+        )
         error_df.Error = error_df.Error.round(3)
-        
-        fig = px.bar(data_frame=error_df, x='Metric', y='Error', color='Metric', text='Error')
-        fig.update_traces(texttemplate='%{text}', textposition='outside')
+
+        fig = px.bar(
+            data_frame=error_df,
+            x='Metric',
+            y='Error',
+            color='Metric',
+            text='Error',
+        )
+        fig.update_traces(
+            texttemplate='%{text}',
+            textposition='outside',
+        )
         fig.update_layout(
-            title=dict(text='Error Metric Results', font=dict(size=20)),
-            template='plotly_dark', 
+            title=dict(
+                text='Error Metric Results',
+                font=dict(size=20),
+            ),
+            template='plotly_dark',
             margin=dict(r=10, l=10, b=10),
         )
         return fig
--- app.py
+++ app.py
@@ -12,25 +12,36 @@ from anomaly_detection import ADetector
 
 host, port = '127.0.0.1', '8086'
 dataset_name = 'Reaction Wheel Temperature'
-db_name = 'sensors_data' 
-retention_policie='rp_temp'
+db_name = 'sensors_data'
+retention_policie = 'rp_temp'
 field_name = 'downsampled_temp'
 
 
 def get_data(client, retention_policie, field_name):
-    df = client.query(f'SELECT * FROM "{retention_policie}"."{field_name}"')[field_name]
+    df = client.query(
+        f'SELECT * FROM "{retention_policie}"."{field_name}"'
+    )[field_name]
     df.index = df.index.tz_localize(None)
     df.reset_index(inplace=True)
     df.columns = ['ds', 'y']
     return df
 
-# init connection 
-myclient = DataFrameClient(host=host, port=port, database=db_name)
-dataframe = get_data(client=myclient, retention_policie=retention_policie, field_name=field_name)
+
+# init connection
+myclient = DataFrameClient(
+    host=host,
+    port=port,
+    database=db_name,
+)
+dataframe = get_data(
+    client=myclient,
+    retention_policie=retention_policie,
+    field_name=field_name,
+)
 
 # create anomaly detector
 detector = ADetector(name='TRW1MT (C)')
-params = ['D'] #['Y']
+params = ['D']  # ['Y']
 detector.update_model(df=dataframe, params=params)
 
 # Start the app
@@ -48,29 +59,33 @@ app.layout = html.Section([
     html.Div(className='create_container', children=[
         # options
         html.Div(className='three columns card-display', children=[
-            html.P('Model Controls', className='label-control'), 
+            html.P('Model Controls', className='label-control'),
 
             html.P('Model Parameters', className='label'),
             dcc.Checklist(
                 id='model_parameters_checklist',
                 options=[{
-                    'label':interval,
-                    'value':interval[0]
-                } for interval in ['Daily Seasonality', 'Weekly Seasonality', 'Yearly Seasonality']],
+                    'label': interval,
+                    'value': interval[0]
+                } for interval in [
+                    'Daily Seasonality',
+                    'Weekly Seasonality',
+                    'Yearly Seasonality',
+                ]],
                 value=params,
                 className='model-parameters-checklist',
-                style={'color':'white', 'margin-left':'20px'}
+                style={'color': 'white', 'margin-left': '20px'}
             ),
 
             html.P('# of Days Forecasted', className='label'),
             dcc.Slider(
                 id='forecast_days_slider',
-                marks={str(days):str(days) for days in [7, 30, 90, 180, 365]},
+                marks={str(days): str(days) for days in [7, 30, 90, 180, 365]},
                 value=7,
                 included=True,
-                min=7, 
-                max=365, 
-                step=1, 
+                min=7,
+                max=365,
+                step=1,
                 updatemode='drag',
                 className='forecast-days-slider'
             ),
@@ -78,12 +93,12 @@ app.layout = html.Section([
             html.P('Model Updating', className='label'),
             dcc.RadioItems(
                 id='updating_interval',
-                options=[{'label':interval, 'value':days} for interval, days in zip(
-                    ['Weekly', 'Monthly', 'Yearly'], 
+                options=[{'label': interval, 'value': days} for interval, days in zip(
+                    ['Weekly', 'Monthly', 'Yearly'],
                     [7, 30, 365]
                 )],
                 value=7,
-                style={'color':'white', 'margin-left':'20px'},
+                style={'color': 'white', 'margin-left': '20px'},
                 className='updating-interval'
             ),
         ]),
@@ -91,13 +106,13 @@ app.layout = html.Section([
         # streaming
         html.Div(className='ten columns card-display', children=[
             dcc.Interval(
-                id='update_chart', 
-                interval=10000, 
+                id='update_chart',
+                interval=10000,
                 n_intervals=0,
             ),
             dcc.Graph(
-                id='timeseries', 
-                config=dict(displayModeBar='hover'), 
+                id='timeseries',
+                config=dict(displayModeBar='hover'),
                 # animate = True,
                 figure=detector.stream_anomaly_plot(series_name=dataset_name),
             ),
@@ -106,26 +121,26 @@ app.layout = html.Section([
 
     html.Div(className='create_container', children=[
         # histogram
-        html.Div(className='four columns card-display', children=[#animate=True
+        html.Div(className='four columns card-display', children=[  # animate=True
             dcc.Graph(
-                id='histogram', 
-                config=dict(displayModeBar='hover'), 
+                id='histogram',
+                config=dict(displayModeBar='hover'),
                 figure=detector.hist_plot()
             ),
         ]),
         # model-components
         html.Div(className='five columns card-display', children=[
             dcc.Graph(
-                id='seasonal_components', 
-                config=dict(displayModeBar='hover'), 
+                id='seasonal_components',
+                config=dict(displayModeBar='hover'),
                 figure=detector.seasonal_components_plot(),
             ),
         ]),
         # error-barchart
         html.Div(className='four columns card-display four columns', children=[
             dcc.Graph(
-                id='error-barchart', 
-                config=dict(displayModeBar='hover'), 
+                id='error-barchart',
+                config=dict(displayModeBar='hover'),
                 figure=detector.metric_plot(),
             ),
         ]),
@@ -138,14 +153,18 @@ app.layout = html.Section([
     Output('histogram', 'figure'),
     Output('seasonal_components', 'figure'),
     Output('error-barchart', 'figure'),
-    
+
     Input('update_chart', 'n_intervals'),
     Input('forecast_days_slider', 'value'),
     Input('model_parameters_checklist', 'value'),
     Input('updating_interval', 'value'),
 )
 def update_graphs(index, period, params, interval):
-    dataframe = get_data(client=myclient, retention_policie=retention_policie, field_name=field_name)
+    dataframe = get_data(
+        client=myclient,
+        retention_policie=retention_policie,
+        field_name=field_name,
+    )
 
     # update model
     days = (dataframe.ds.iloc[-1] - detector.model_init_date).days
@@ -160,11 +179,13 @@ def update_graphs(index, period, params, interval):
     # update streaming data
     detector.predict_dataframe(series=dataframe)
 
-    return [detector.stream_anomaly_plot(series_name=dataset_name), detector.hist_plot(), 
-            detector.seasonal_components_plot(), detector.metric_plot()]
+    return [
+        detector.stream_anomaly_plot(series_name=dataset_name),
+        detector.hist_plot(),
+        detector.seasonal_components_plot(),
+        detector.metric_plot(),
+    ]
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run_server(host='127.0.0.1', port='8050', debug=False)
-
-
